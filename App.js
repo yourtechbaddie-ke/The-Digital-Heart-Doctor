410f3a2b9912a6185161f17e59f73a3f1fa48afe
yourtechbaddie-ke <muhatiaregina68@gmail.com>
2026-04-24 07:01:41 +0300

    Updated App.js with complete React Native navigation setup

--- App.js
+++ App.js
@@ -1,1 +1,28 @@
-// Main entry point with navigation stack setup code here
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './src/screens/LoginScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import BreathingExerciseScreen from './src/screens/BreathingExerciseScreen';
+import SensorScreen from './src/screens/SensorScreen';
+import HistoryScreen from './src/screens/HistoryScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Breathing Exercise" component={BreathingExerciseScreen} />
+        <Stack.Screen name="Sensor" component={SensorScreen} />
+        <Stack.Screen name="History" component={HistoryScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
